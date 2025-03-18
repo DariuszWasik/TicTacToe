41434401979eedcdffd4a8473de88c64d70d18a1
Dario_W <wasik.dariusz24@gmail.com>
2025-03-18 10:06:49 +0100

    repair mechanic with eventListener, make all work only from gameControl.playGame(), add draw mechanism >

--- script2.js
+++ script2.js
@@ -29,10 +29,10 @@ const gameBoard = (function() {
             ||((board[2] == board[4]) && (board[4] == board[6]) && (board[6] != ''))
         ){
             printNewBoard();
-            console.log('you win z checkLines')
-            // scrDisplay.render();
-            return true;}
-            else false
+            return true;
+            
+        }
+            else return false
         }
 
 return {board, checkLines, printNewBoard}
@@ -40,6 +40,7 @@ return {board, checkLines, printNewBoard}
 })()
 
 // function factory which makes players
+
 function createPlayer(name) {
         let won = false;
         let points = 0;
@@ -57,109 +58,109 @@ const player1 = createPlayer('dario')
 const player2 = createPlayer('asia')
 gameBoard.board[1] = ''
 player1.marker = 'x';
-player2.marker = 'yO';
-
+player2.marker = 'o';
 
 
 //gameControl
 const gameControl =(function() {
-            const roundNumber = 0;
-            const moveInRound = 0;
-            let token = 'x';
+            let roundNumber = 0;
+            let moveInRound = 0;
+            let token = player1.marker;
                      
             const changeToken  = function() {
-             this.token == 'x' ? this.token = 'o' : this.token = 'x'
+              return gameControl.token == player1.marker ? gameControl.token = player2.marker : gameControl.token = player1.marker
             } 
+            
+            const setToken = function() {
+                if ((gameControl.roundNumber)%2 === 0){
+                    gameControl.token = player1.marker
+                }
+                else {
+                    gameControl.token = player2.marker
+                }
+            }
 
-            const playRound = function() {
-                ++this.moveInRound;
-                if((this.roundNumber + this.moveInRound)%2 !== 0){
-                    this.token = player1.marker;
-                    // gameBoard.checkLines()
-                    // getChoice(player1.name)
-                    // makeMove(player1);
+            // const playRound = function() {
+            //     if ((moveInRound)%2 == 0){
+            //         setToken()
+            //         console.log('00', token)
+            //     }
+            //     else {
+            //         changeToken()
+            //         console.log('01', gameControl.token)
+            //     }
+            // }
+            
+            function makeMove() {
+                ++moveInRound;
+                console.log(moveInRound, gameControl.roundNumber)
+                if(gameControl.token == player1.marker){
                     addPointIfWon(player1)
                 }
-                else{
-                    gameControl.token = player2.marker;
-                    // gameBoard.checkLines()
-                    // getChoice(player2.name)
-                    // makeMove(player2);
+                else {
                     addPointIfWon(player2)
                 }
+                
             }
 
             function checkPoints(playa) {
                 if (playa.points > 2){
-                    console.log(`${playa.name} you have won the game`) }
+                    console.log('loremsaffjkahkjshdakhsajkhjkhkjkjdsjkshfdkfdjhfddfdk')
+                    scrDisplay.grid.innerHTML = '<p>`${playa.name} you have won the game`</p>' }
              }
-            
-
-            // function checkInput(i) {
-            //     while(acceptedValues.includes(i)==false){
-            //         i = prompt('this value is not allowed enter another x.y')
-            //     };
-            //     const values = i.split('.')
-            //     if (gameControl.takenValues.includes(i)){
-            //         i = prompt(`${gameControl.currentNick} ${values} already taken enter another x.y`)
-            //         checkInput(i);
-            //     }
-            //     else {
-            //         gameControl.takenValues.push(i);
-            //         gameControl.chosenX = values[0];
-            //         gameControl.chosenY = values[1];}
-            //     }
-
-            // function getChoice(nick) {
-            //     gameControl.currentNick = nick;
-            //     let input ='' 
-            //     if ((gameControl.takenValues.length == 9) && (gameBoard.checkLines() != true)) {
-            //         console.log ('its a draw')
-            //         gameBoard.printNewBoard()
-            //         gameControl.takenValues = [];
-            //         gameControl.playRound()}; 
-            //         input = prompt(`${nick} enter x.y`);
-            //         checkInput(input);
-            //     }  
-
+     
             function addPointIfWon(playa) {
-                console.log('funkcja dodawania punktow')
                 if(gameBoard.checkLines()==true){
-                    debugger
-                console.log('true!!!!!!!!!')
-                    playa.givePoint()
-                    checkPoints(playa);
+                    console.log('true!!!!!!!!!')
+                    ++gameControl.roundNumber;
+                    moveInRound = 0;
+                    playa.givePoint();
                     gameBoard.printNewBoard();
-                    scrDisplay.render();
+                    setToken();
+                    scrDisplay.execute()
+                    checkPoints(playa);
                 }
-                else if (gameBoard.checkLines() == false){
-                    console.log('chekclines sa flaszywe')
+                else if ((gameBoard.checkLines() == false) && (!gameBoard.board.includes(''))){
+                    console.log('its a draw')
+                    ++gameControl.roundNumber;
+                    moveInRound = 0;
+                    gameBoard.printNewBoard();
+                    setToken();
+                    scrDisplay.execute()
+                    // changeToken()
                 }
-            }
-            
-            const makeMove = function() {
-                // gameBoard.board[gameControl.chosenX][gameControl.chosenY] = gameControl.token
-                console.table(gameBoard.board);
+                else {
+                    changeToken()
+                }
             }
             
             const playGame = function() {
-                while((player1.points < 3) && (player2.points <3)){
-                    this.playRound()
+                if((player1.points < 3) && (player2.points <3)){
+                    scrDisplay.execute()
+             
+                   // playRound()
                 }
-                console.log('wanna play again?')
+                else prompt('skks')
             }
+
             
-            return {moveInRound, changeToken, roundNumber, token, playGame,playRound,
-                addPointIfWon, makeMove, checkPoints}
+return {moveInRound, changeToken, roundNumber, token, playGame,
+                addPointIfWon, checkPoints, makeMove}
             })()
             
-            
-            //////////////////////////////////////////////////////////////////////////////////////////
-            let everyBtn = []
     
     const scrDisplay = (function() {
+
+        let score1 = document.querySelector('.score1');
+        let score2 = document.querySelector('.score2');
         const grid = document.querySelector('.grid');
-        
+        let everyBtn = []
+
+        function updatePoints() {
+        score1.innerText = player1.points
+        score2.innerText = player2.points
+        } 
+
         function drawGrid(x) {
             let el = document.createElement('button');
             el.innerText = x;
@@ -171,34 +172,20 @@ const gameControl =(function() {
         grid.innerHTML = '';
         gameBoard.board.forEach((x)=> drawGrid(x));
         everyBtn = document.querySelectorAll('.btnGrid')
-        console.log('zrenderowalem');
-        return everyBtn
-        
+        updatePoints()
     }
     
     function addEventListenerToEmptyButtons(){
-        console.log('uruchomilem iventlistnera')
         for (let x=0; x<9; x++){
             if (everyBtn[x].innerText == ''){
-                everyBtn[x].addEventListener('click', () => gameControl.playRound());
-                everyBtn[x].addEventListener('click', () => {everyBtn[x].innerText = gameControl.token});
                 everyBtn[x].addEventListener('click', () => {gameBoard.board[x] = gameControl.token});
+                everyBtn[x].addEventListener('click', () => {everyBtn[x].innerText = gameControl.token});
+                everyBtn[x].addEventListener('click', () => gameControl.makeMove());
+                everyBtn[x].addEventListener('click', () => execute());
                 everyBtn[x].addEventListener('click', () => console.log(gameBoard.board));
-                
-                // everyBtn[x].addEventListener('click', () => gameControl.changeToken());
-                // everyBtn[x].addEventListener('click', () => render());
-                
             }
             else everyBtn[x].style.backgroundColor = 'red'
         }
-        
-        
-    }
-    
-    function clickBtn(a) {
-        everyBtn[a].innerText = gameControl.token;
-        gameBoard.board[a] = gameControl.token;
-        gameControl.changeToken();
     }
 
 
@@ -206,13 +193,10 @@ const gameControl =(function() {
         render()
         addEventListenerToEmptyButtons();
     }
-    execute()
-    
-    
-    return {render, grid, addEventListenerToEmptyButtons}
+
+return {render, grid, addEventListenerToEmptyButtons, score1, score2, execute, everyBtn}
     
 })()
 
 
-    const score = document.querySelector('.score');
-    score.innerText = 3
+gameControl.playGame()
